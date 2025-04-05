dd9bdaf3e9caf0b17dbf9ada8478939dcbdc4a49
Niko Dale <cnotsomark@gmail.com>
2025-04-05 12:49:18 -0400

    Fixed license dates for LazyPages Files.
    Signed-off-by: Niko Dale <cnotsomark@gmail.com>

--- kord-extensions/src/test/kotlin/dev/kordex/core/pagination/pages/lazy/LazyPagesTest.kt
+++ kord-extensions/src/test/kotlin/dev/kordex/core/pagination/pages/lazy/LazyPagesTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyrighted (Kord Extensions, 2024). Licensed under the EUPL-1.2
+ * Copyrighted (Kord Extensions, 2025). Licensed under the EUPL-1.2
  * with the specific provision (EUPL articles 14 & 15) that the
  * applicable law is the (Republic of) Irish law and the Jurisdiction
  * Dublin.
